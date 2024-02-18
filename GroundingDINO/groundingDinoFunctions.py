b1204ba647a81530339f185969e501f18489e30a
omersela10 <90277998+omersela10@users.noreply.github.com>
2024-02-18 12:22:43 +0200

    resize all coffee images

--- GroundingDINO/groundingDinoFunctions.py
+++ GroundingDINO/groundingDinoFunctions.py
@@ -7,6 +7,7 @@ import numpy as np
 import torch
 import random
 import cv2
+from PIL import Image
 from groundingdino.util.inference import load_model, load_image, predict, annotate
 
 def load_groundingdino():
@@ -52,17 +53,40 @@ def object_detection(model, image_path, text_prompt="coffee cup", box_treshold=0
     return boxes, logits, phrases
 
 def resize_image(image_path, new_image_path, size=(224, 224)):
-    image = cv2.imread(image_path)
-    image = cv2.resize(image, size)
-    cv2.imwrite(new_image_path, image)
-    return new_image_path
+    try:
+        # Change permissions for the file
+        os.chmod(image_path, 0o777)
+        # Open the image file
+        with Image.open(image_path) as img:
+            # Resize the image
+            resized_img = img.resize(size)
+            # Save the resized image to the output path
+            resized_img.save(new_image_path)
+            print(f"Resized image saved to {new_image_path}")
+    except FileNotFoundError:
+        print("Input image file not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    model = load_groundingdino()
-    image_path = os.path.join(os.getcwd(),'..\\Coffee_photos\\images\\1-50')
+    # model = load_groundingdino()
+    # image_path = os.path.join(os.getcwd(),'..\\Coffee_photos\\images\\1-50')
+    # for filename in os.listdir(image_path):
+    #     if filename.endswith(".jpg"):
+    #         new_image_path = os.path.join(image_path,filename)
+    #         object_detection(model, new_image_path)
+    #resize_image(image_path, new_image_path)
+
+    image_path = "C:\\git\\other_coffeeML\\coffeeML\\Coffee_photos\\resized_images\\all_images_before_resize"
+    new_image_path = "C:\\git\\other_coffeeML\\coffeeML\\Coffee_photos\\resized_images\\all_images_after_resize_224X224"
     for filename in os.listdir(image_path):
         if filename.endswith(".jpg"):
-            new_image_path = os.path.join(image_path,filename)
-            object_detection(model, new_image_path)
-    #resize_image(image_path, new_image_path)
+            this_image_path = os.path.join(image_path,filename)
+            output_image_path = os.path.join(new_image_path, filename)
+            resize_image(this_image_path, output_image_path, (224, 224))
+    
+    if len(os.listdir(image_path)) == len(os.listdir(new_image_path)):
+        print("All images were resized successfully")
+    else:
+        print("Some images were not resized")
 
